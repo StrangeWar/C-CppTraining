eadb3d37e3024e0f14ce701b531e1d01650781f3
vivek <vivekrockss111@gmail.com>
2024-09-02 12:52:37 +0530

    fabionacci series using recursion

---
+++ DSA/Recursion/fabionacci.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int fabionacci(int num){
+
+    if(num == 0)
+        return 0;
+
+    else if(num == 1)
+        return 1;
+
+    return fabionacci(num-1) + fabionacci(num-2);
+    
+}
+
+
+int main() {
+
+    cout << fabionacci(6);
+    cout << endl;
+    //fabionacci(6);
+
+    int i = 0;
+    while(i != 10){
+        cout << fabionacci(i) << " ";
+        i++;
+    }
+
+    return 0;
+}
