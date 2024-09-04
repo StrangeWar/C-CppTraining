bc2c4a6e0f2dc06da3e0173a836addc57e6b60b9
vivek <vivekrockss111@gmail.com>
2024-09-04 11:04:42 +0530

    "Updated NOTES.txt with information on trees and added a new tree.cpp file with a basic node class implementation."

---
+++ DSA/Tree/tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left, *right;
+
+    node(int val):data(val), left(NULL), right(NULL){};
+};
+
+int main() {
+
+    node* root = new node(10);
+    root->left = new node(20);
+    root->right = new node(30);
+    root->left->left = new node(40);
+    root->left->right = new node(50);
+    root->right->left = new node(60);
+    root->right->right = new node(70);
+
+    return 0;
+}
