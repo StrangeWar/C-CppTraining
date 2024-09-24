37fe9582dc3d32aa012f04dc189e4ee405776fd0
vivek <vivekrockss111@gmail.com>
2024-09-24 23:43:07 +0530

    "Updated main.c and main.cpp files to implement frequency counting of array elements, and modified main.exe accordingly. And implemented selection sort"

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
-int main()
-{
-    
-return 0;
+#include <stdio.h>
+
+int main() {
+
+    int arr[10] = {1 ,2 ,3, 2, 1, 3, 4, 7, 7, 3};
+    int hash[11] = {0};
+
+    for(int i=0; i<=10; i++){
+        hash[arr[i]] += 1;
+    }
+
+    for(int i=1; i<=10; i++){
+        printf("%d = %d ", i, hash[i]);
+    }
+
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 
+int main() {
 
-int main(){
+    int arr[10] = {122,122, 1, 23, 33, 23, 23, 33, 1, 1};
 
-    cout<<pow(6 ,0.5);
+    map<int, int> mp;
+    for (int i = 0; i < 10; i++) {
+        mp[arr[i]]++;
+    
+    }
+
+    for(auto it: mp){
+        cout << it.first << " " << it.second << endl;
+    }
 
     return 0;
 }
